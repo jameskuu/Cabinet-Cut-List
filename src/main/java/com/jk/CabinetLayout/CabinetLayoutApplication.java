429204615628cdcbe8098a9d8e7ce01172a6ee5e
James <develop@kuusisto.com>
2020-11-15 18:22:55 -0700

    Added Maven Spring Boot Project files

---
+++ src/main/java/com/jk/CabinetLayout/CabinetLayoutApplication.java
@@ -0,0 +1,13 @@
+package com.jk.CabinetLayout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetLayoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinetLayoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jk/CabinetLayout/CabinetLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.CabinetLayout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinetLayoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
